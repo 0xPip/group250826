191758e5fd99b699844fd2ed9a30809a3e9b3ee8
Artem <artemandpol@gfmail.com>
2026-09-04 19:05:17 +0300

    test utils

--- app/project/tests/test_utils.py
+++ app/project/tests/test_utils.py
@@ -11,3 +11,10 @@ def test_is_nuber_positive_1():
     expected = True
     actual = is_nuber_positive(number)
     assert expected is actual
+
+
+def test_is_nuber_positive():
+    number = 5
+    expected = True
+    actual = is_nuber_positive(number)
+    assert expected is actual
